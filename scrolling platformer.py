3838d190469ca43573a4076c64c0e24fc48cd6b8
qwerttiger1 <qwerttiger123456789@gmail.com>
2020-08-02 20:07:05 -0400

    load image and level and add player file

--- scrolling platformer.py
+++ scrolling platformer.py
@@ -2,3 +2,5 @@ import pygame
 pygame.init()
 pygame.display.set_mode([700,700])
 pygame.display.set_caption("Scrolling platformer")
+level=1
+player=pygame.image.load("C:/Users/Rainbow/Documents/GitHub/scrolling-platformer/player.png")
