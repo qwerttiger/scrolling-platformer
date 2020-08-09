6dbd39ea430e90905a8cf3848da5149833f12d6e
qwerttiger1 <qwerttiger123456789@gmail.com>
2020-08-09 16:49:57 -0400

    startthing and pause (startthing copied from my other project with some minor changes)

--- scrolling platformer.py
+++ scrolling platformer.py
@@ -79,6 +79,123 @@ def down(): #go down
   while bool(groundmask.overlap_area(top,(screenposx+300,screenposy+299))): #while touching ground
     screenposy+=1 #go down
 
+def drawtext(text,colour,size=30,pos=(350,100)): #draws a single piece of text
+  screen.blit(pygame.font.SysFont("arial",size).render(text,True,colour),(pos[0]-round(pygame.font.SysFont("arial",size).render(text,True,colour).get_width()/2),pos[1]-pygame.font.SysFont("arial",size).render(text,True,colour).get_height()/2))
+
+def startthing(): #the thing at the start
+  global playerr,playerl #set everything to be global
+  
+  ctime=time.time() #current time
+  
+  screen.fill((255,255,255)) #fill screen
+  drawtext("scrolling platformer",(0,0,0),50) #draw text
+  
+  #drawing play button
+  pygame.draw.rect(screen,(0,0,0),pygame.Rect((300,300),(100,100)),1)
+  pygame.draw.line(screen,(0,0,0),(336,325),(336,375))
+  pygame.draw.line(screen,(0,0,0),(336,325),(379,350))
+  pygame.draw.line(screen,(0,0,0),(336,375),(379,350))
+  pygame.draw.rect(screen,(0,0,0),pygame.Rect((100,300),(100,100)),1)
+  screen.blit(playerr,(125,325)) #draw player
+
+  pygame.display.flip() #flip screen
+  keep_going=True #set keep_going
+
+  while keep_going: #while you keep going
+    for event in pygame.event.get(): #for every event
+      if event.type==pygame.QUIT: #if quit
+        pygame.quit() #quit
+        sys.exit() #exit
+
+      if event.type==pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pos()[0]>=300 and pygame.mouse.get_pos()[0]<=400 and pygame.mouse.get_pos()[1]>=300 and pygame.mouse.get_pos()[1]<=400: #if you press play
+        keep_going=False #go to the main game
+
+      if event.type==pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pos()[0]>=100 and pygame.mouse.get_pos()[0]<=300 and pygame.mouse.get_pos()[1]>=300 and pygame.mouse.get_pos()[1]<=400:
+        #draw the screen
+        screen.fill((255,255,255))
+        pygame.draw.rect(screen,(0,0,0),pygame.Rect((175,325),(50,50)))
+        pygame.draw.rect(screen,(255,0,0),pygame.Rect((225,325),(50,50)))
+        pygame.draw.rect(screen,(0,255,0),pygame.Rect((275,325),(50,50)))
+        pygame.draw.rect(screen,(0,0,255),pygame.Rect((325,325),(50,50)))
+        pygame.draw.rect(screen,(255,255,0),pygame.Rect((375,325),(50,50)))
+        pygame.draw.rect(screen,(255,0,255),pygame.Rect((425,325),(50,50)))
+        pygame.draw.rect(screen,(0,255,255),pygame.Rect((475,325),(50,50)))
+        pygame.display.flip() #flip display
+        kep_going=True #lol keep_going but... yeah
+        while kep_going: #while keep going
+          for event in pygame.event.get(): #for every event
+            if event.type==pygame.QUIT: #if you quit
+              pygame.quit() #quit
+              sys.exit() #exit
+            if event.type==pygame.MOUSEBUTTONDOWN: #if you click
+              pos=pygame.mouse.get_pos() #you find the position of the mouse
+
+              if pos[1]>=325 and pos[1]<=375: #if you click in the region horizontal region where it works
+                if pos[0]>=175 and pos[0]<225: #if you click on black
+                  for posx in range(50): #for every column
+                    for posy in range(50): #for every pixel in that column
+                      if playerr.get_at((posx,posy)) not in [(255,255,255),(254,254,254)]: #if it is not blank
+                        playerr.set_at((posx,posy),(0,0,0)) #set that pixel to black
+                #the same for the other ones but with different colours
+
+                if pos[0]>=225 and pos[0]<275:
+                  for posx in range(50):
+                    for posy in range(50):
+                      if playerr.get_at((posx,posy)) not in [(255,255,255),(254,254,254)]:
+                        playerr.set_at((posx,posy),(255,0,0))
+
+                if pos[0]>=275 and pos[0]<325:
+                  for posx in range(50):
+                    for posy in range(50):
+                      if playerr.get_at((posx,posy)) not in [(255,255,255),(254,254,254)]:
+                        playerr.set_at((posx,posy),(0,255,0))
+
+                if pos[0]>=325 and pos[0]<375:
+                  for posx in range(50):
+                    for posy in range(50):
+                      if playerr.get_at((posx,posy)) not in [(255,255,255),(254,254,254)]:
+                        playerr.set_at((posx,posy),(0,0,255))
+
+                if pos[0]>=375 and pos[0]<425:
+                  for posx in range(50):
+                    for posy in range(50):
+                      if playerr.get_at((posx,posy)) not in [(255,255,255),(254,254,254)]:
+                        playerr.set_at((posx,posy),(255,255,0))
+
+                if pos[0]>=425 and pos[0]<475:
+                  for posx in range(50):
+                    for posy in range(50):
+                      if playerr.get_at((posx,posy)) not in [(255,255,255),(254,254,254)]:
+                        playerr.set_at((posx,posy),(255,0,255))
+
+                if pos[0]>=475 and pos[0]<525:
+                  for posx in range(50):
+                    for posy in range(50):
+                      if playerr.get_at((posx,posy)) not in [(255,255,255),(254,254,254)]:
+                        playerr.set_at((posx,posy),(0,255,255))
+
+                playerl=pygame.transform.flip(playerr,True,False) #set the new playerl
+                
+                for x in [playerr,playerl]: #for everything in this list
+                  x.set_colorkey((255,255,255)) #set the colourkey
+
+              #draw the things
+              screen.fill((255,255,255))
+              drawtext("elements | a platformer",(0,0,0),50)
+              pygame.draw.rect(screen,(0,0,0),pygame.Rect((300,300),(100,100)),1)
+              pygame.draw.line(screen,(0,0,0),(336,325),(336,375))
+              pygame.draw.line(screen,(0,0,0),(336,325),(379,350))
+              pygame.draw.line(screen,(0,0,0),(336,375),(379,350))
+              pygame.draw.rect(screen,(0,0,0),pygame.Rect((100,300),(100,100)),1)
+              screen.blit(playerr,(125,325))
+
+              kep_going=False #exit this loop
+              pygame.display.flip() #flip screen
+
+#real game
+
+startthing()
+
 while True: #level loop
   loadlevel() #load the level
   reset() #reset position
@@ -169,6 +286,9 @@ while True: #level loop
     
     if lava or keys[pygame.K_r] or screenposy>=3700: #if touch lava
       reset() #reset level
+
+    if keys[pygame.K_p]:
+      startthing()
     
     pygame.display.flip() #flip screen
     
